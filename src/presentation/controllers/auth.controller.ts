48a0442c7978cb5cf70f30153dae54c67f73605f
mahmoud661 <mahmoudzuriqi8@gmail.com>
2025-04-14 20:47:59 +0300

    refactor

--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -12,24 +12,24 @@ export class AuthController {
 
   async register(req: Request, res: Response) {
     try {
-        //TODO: validation middleware for not allowing empty fields
-      if (!req.body || !req.body.email || !req.body.password || !req.body.username) {
+      //TODO: validation middleware for not allowing empty fields
+      if (
+        !req.body ||
+        !req.body.email ||
+        !req.body.password ||
+        !req.body.username
+      ) {
         return res.status(400).json({
-          error: "Missing required fields. Username, email and password are required."
+          error:
+            "Missing required fields. Username, email and password are required.",
         });
       }
-
 
       const user: PostUserDTO = req.body;
       const result = await this.userService.register(user);
 
-     
-
       res.status(201).json(result);
-    } 
-    
-    
-    catch (error) {
+    } catch (error) {
       Logger.Error(error);
       res.status(500).json({
         error: error instanceof Error ? error.message : String(error),
@@ -46,17 +46,16 @@ export class AuthController {
           error: "Missing required fields. Email and password are required.",
         });
       }
-       
-      const user = await this.userService.login({email, password});
-     
+
+      const user = await this.userService.login({ email, password });
 
       if (!user) {
         return res.status(401).json({
           error: "Invalid email or password.",
         });
       }
-      const tokenValue = token( user.username, user.email);
-      
+      const tokenValue = token(user.username, user.email);
+
       res.status(200).json(tokenValue);
     } catch (error) {
       Logger.Error(error);
